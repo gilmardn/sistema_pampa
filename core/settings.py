8d7b3348d9e9787219982d589064ecb21e7538b9
gilmardn <gilmar@cotrijuc.com.br>
2024-09-04 22:14:07 -0300

    Atualiza senha BD

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
 
 ALLOWED_HOSTS = ['.pythonanywhere.com', "*"]
 
